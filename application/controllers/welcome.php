05548fd89d46126567ab550303709e0a068a6670
Daniel Sont <dan.sont@gmail.com>
2014-04-19 21:34:06 +0800

    Data model hmm

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -21,6 +21,57 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+
+
+    public function register_post()
+    {
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $data = $this->input->post('data');
+
+        $this->load->model('auth_model');
+
+        $success = $this->auth_model->create($username, $password, $data);
+
+        $result = [
+            'success' => $success
+        ];
+
+        echo json_encode($result);
+    }
+
+    public function token_get()
+    {
+        $username = $this->input->get('username');
+        $password = $this->input->get('password');
+        $app_id = $this->input->get('app_id');
+
+        $this->load->model('auth_model');
+
+        $user = $this->auth_model->login($username, $password);
+
+        $result = ['success' => false];
+
+        if (!empty($user)) {
+            $token = $this->auth_model->get_token($user['id']);
+            $result = [
+                'success' => $user,
+                'token' => $token
+            ];
+        }
+
+        echo json_encode($result);
+    }
+
+    public function data_get()
+    {
+        $app_id = $this->input->get('app_id');
+        $token = $this->input->get('token');
+
+        $this->load->model('auth_model')
+        $this->auth_model->get_data($app_id, $token);
+
+    }
 }
 
 /* End of file welcome.php */
---
+++ application/models/auth_model.php
@@ -0,0 +1,39 @@
+<?php
+
+class Auth_model extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function create($username, $password, $data)
+    {
+        return $this->db->insert([
+            'username' => $username,
+            'password' => md5($password),
+            'data' => json_encode($data);
+        ]);
+    }
+
+    public function login($username, $password)
+    {
+        return $this->db->query('SELECT username, id FROM users WHERE username = ? AND password = ?', [$username, $password])->row_array();
+    }
+
+    public function get_token($app_id, $user_id)
+    {
+        return $this->db->query('SELECT token FROM tokens WHERE app_id = ? AND user_id = ?', [$app_id, $user_id]);
+    }
+
+    public function get_data($app_id, $token)
+    {
+        return $this->db->query('SELECT data from users INNER JOIN tokens on tokens.user_id = users.id WHERE app_id = ? AND token = ?' [$app_id, $token]);
+    }
+
+    function update_entry()
+    {
+        $this->db->update('entries', $this, array('id' => $_POST['id']));
+    }
+
+}
