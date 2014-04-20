505a64a4fa00e902bd9f57cbbfa5949ee2e1ceb0
Daniel Sont <dan.sont@gmail.com>
2014-04-20 10:06:19 +0800

    Great

--- application/models/auth_model.php
+++ application/models/auth_model.php
@@ -23,12 +23,12 @@ class Auth_model extends CI_Model {
 
     public function get_token($app_id, $user_id)
     {
-        return $this->db->query('SELECT token FROM tokens WHERE app_id = ? AND user_id = ?', [$app_id, $user_id]);
+        return $this->db->query('SELECT token FROM tokens WHERE app_id = ? AND user_id = ?', [$app_id, $user_id])->row_array();
     }
 
     public function get_data($app_id, $token)
     {
-        return $this->db->query('SELECT data from users INNER JOIN tokens on tokens.user_id = users.id WHERE app_id = ? AND token = ?' [$app_id, $token]);
+        return $this->db->query('SELECT data from users INNER JOIN tokens on tokens.user_id = users.id WHERE app_id = ? AND token = ?', [$app_id, $token])->row_array();
     }
 
     function update_entry()
