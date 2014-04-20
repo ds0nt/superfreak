61c36ecfe23f8b9c45fd1cdaf4a429a91a341f87
Daniel Sont <dan.sont@gmail.com>
2014-04-20 13:02:20 +0800

    Great

--- ui/superfreak-button.js
+++ ui/superfreak-button.js
@@ -7,9 +7,12 @@ function superfreakauth() {
     window.location = 'http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/superfreak/welcome/userauth?redirect=' + encodeURI(window.location.href) + '&token=' + token;
 }
 function superfreakinit() {
-    document.getElementById('superfreakbtn').innerHTML = '<button onclick="javascript:superfreakauth();">AUTH ME WITH SUPERFREAK</button>';
-    var user_token = window.location.search.replace( "?token=", "" );
-    if (user_token !== '')
+    var user_token = window.location.search.replace( "?token=", "");
+    if (user_token.length === 64) {
         onSuperFreakAuth(user_token);
+        document.getElementById('superfreakbtn').innerHTML = '<button onclick="javascript:superfreakauth();">SUPERFREAKYNESS!</button>';
+    } else {
+        document.getElementById('superfreakbtn').innerHTML = '<button onclick="javascript:superfreakauth();">AUTH ME WITH SUPERFREAK</button>';
+    }
 }
 document.addEventListener('DOMContentLoaded', superfreakinit, false);
