574622d1353a3cdfab0d144e9d05485c9b7c5e66
Daniel Sont <dan.sont@gmail.com>
2014-04-20 14:48:27 +0800

    Great

--- ui/superfreak-button.js
+++ ui/superfreak-button.js
@@ -3,7 +3,8 @@ var onSuperFreakAuth = function(user_token) {
 };
 
 function superfreakauth() {
-    window.location = 'http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/superfreak/welcome/userauth?redirect=' + encodeURI(window.location.href) + '&token=' + token;
+    var app_token = document.getElementById('superfreak-app-token').value;
+    window.location = 'http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/superfreak/welcome/userauth?redirect=' + encodeURI(window.location.href) + '&token=' + app_token;
 }
 
 function superfreakinit() {
@@ -11,7 +12,7 @@ function superfreakinit() {
     var user_token = window.location.search.replace( "?token=", "");
     if (user_token.length === 64) {
         onSuperFreakAuth(user_token);
-        document.getElementById('superfreakbtn').innerHTML = '<iframe src="http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/superfreak/welcome/userinfo?app_token='+token+'&user_token=' + user_token + '"></iframe>';
+        document.getElementById('superfreakbtn').innerHTML = '<iframe src="http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/superfreak/welcome/userinfo?app_token='+app_token+'&user_token=' + user_token + '"></iframe>';
     } else {
         document.getElementById('superfreakbtn').innerHTML = '<button onclick="javascript:superfreakauth();">AUTH ME WITH SUPERFREAK</button>';
     }
