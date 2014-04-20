9cf21941296f45c6a5d8a7e7194e80da7e5cb04a
Daniel Sont <dan.sont@gmail.com>
2014-04-20 09:38:55 +0800

    SuperFreak API

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,21 +2,6 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('welcome_message');
@@ -68,11 +53,8 @@ class Welcome extends CI_Controller {
         $app_id = $this->input->get('app_id');
         $token = $this->input->get('token');
 
-        $this->load->model('auth_model')
+        $this->load->model('auth_model');
         $this->auth_model->get_data($app_id, $token);
 
     }
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
