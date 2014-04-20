8a54b035b73e3e138817aafb76ad88e18099d8d3
Daniel Sont <dan.sont@gmail.com>
2014-04-20 14:23:45 +0800

    Great

--- ui/js/app.js
+++ ui/js/app.js
@@ -5,7 +5,7 @@ var data = {
 var appinstructions = function(data) {
     dust.render("appinstructions", {
         token: data.token,
-        tokencode: '<input type="hidden" id="superfreak-app-token" value="'+token+'">
+        tokencode: '<input type="hidden" id="superfreak-app-token" value="'+data.token+'">
     <div id="superfreakbtn"></div>
     <script type="text/javascript" src="http://ec2-54-254-252-98.ap-southeast-1.compute.amazonaws.com/ui/superfreak-button.js"></script>'
     }, function(err, out) {
